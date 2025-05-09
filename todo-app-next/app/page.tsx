79d3e5b3f25c91928689c91b0d94aa3ac542140d
Dimitri Guerroue <dimitri.guerroue@gmail.com>
2025-05-09 11:31:19 +0200

    init todo-app-next

---
+++ todo-app-next/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="">
+      <h1>NextJS Todo List</h1>
+    </div>
+  );
+}
---
+++ todo-app-next/eslint.config.mjs
@@ -0,0 +1,33 @@
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import { FlatCompat } from "@eslint/eslintrc";
+import tseslint from 'typescript-eslint'
+import typescriptPlugin from "@typescript-eslint/eslint-plugin";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+const eslintConfig = tseslint.config(
+  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    plugins: {
+      "@typescript-eslint": typescriptPlugin, // 👈 ENREGISTREMENT
+    },
+    rules: {
+      "@typescript-eslint/no-unused-vars": [
+        "warn",
+        {
+          argsIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+          ignoreRestSiblings: true,
+        },
+      ],
+    },
+  }
+);
+
+export default eslintConfig;
